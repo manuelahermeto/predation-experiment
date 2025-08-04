aeca239d2b62cf1f91eba407ea13823830ddae15
Manuela <manuela.hermeto@gmail.com>
2025-08-04 16:32:12 -0300

    script for testing

--- scripts/test.R
+++ scripts/test.R
@@ -1,38 +1,15 @@
 
 em2test <- emmeans(f2, ~ guild | treatment, type = "response")
-summary(ef2test)
+summary(em2testdf)
 plot(ef2test)
+
 
 # Converter emmeans para data frame
 ef2testdf <- as.data.frame(ef2test)
 
 # Ocultar barras com UCL == 1
-ef2testdf$asymp.UCL[ef2testdf$asymp.UCL >= 0.999] <- NA
-ef2testdf$asymp.LCL[is.na(ef2testdf$asymp.UCL)] <- NA
-
-# Plot personalizado
-ggplot(ef2testdf, aes(x = prob, y = guild, color = treatment, group = treatment)) +
-  geom_point(position = position_dodge(0.3), size = 3) +
-  geom_errorbar(aes(xmin = asymp.LCL, xmax = asymp.UCL),
-                position = position_dodge(0.3),
-                width = 0.2,
-                size = 1.5,
-                alpha = 0.5) +  #transparência da linha
-  scale_x_continuous(limits = c(0, 0.8)) +
-  labs(x = NULL, y = NULL) +
-  theme_gray()
-
-ggplot(ef2testdf, aes(x = prob, y = guild, color = guild, group = guild)) +
-  geom_point(position = position_dodge(0.3), size = 3) +
-  geom_errorbar(aes(xmin = asymp.LCL, xmax = asymp.UCL),
-                position = position_dodge(0.3),
-                width = 0.2,
-                size = 1.2,
-                alpha = 0.5) +
-  scale_x_continuous(limits = c(0, 0.8)) +
-  labs(x = NULL, y = NULL) +
-  facet_wrap(~ treatment, nrow = 1, scales = "fixed") +  # ⬅️ facetas na horizontal com mesmo eixo x
-  theme_gray()
+ef2df$asymp.UCL[ef2df$asymp.UCL >= 0.999] <- NA
+ef2df$asymp.LCL[is.na(ef2df$asymp.UCL)] <- NA
 
 ###Agrupar por tratamento
 ggplot(ef2testdf, aes(y = guild, x = prob, color = guild, group = guild)) +
@@ -53,7 +30,8 @@ ggplot(ef2testdf, aes(y = guild, x = prob, color = guild, group = guild)) +
 
 #### Agrupar por guilda
 #Colocar tratamentos na ordem
-ggplot(em2testdf, aes(y = treatment, x = prob, color = treatment, group = treatment)) +
+
+ggplot(ef2test, aes(y = treatment, x = prob, color = treatment, group = treatment)) +
   geom_point(position = position_dodge(width = 0.3), size = 3) +
   geom_errorbarh(aes(xmin = asymp.LCL, xmax = asymp.UCL),
                  position = position_dodge(width = 0.3),
@@ -70,11 +48,16 @@ ggplot(em2testdf, aes(y = treatment, x = prob, color = treatment, group = treatm
         legend.position = "none")
 
 
+str(em2df$treatment)
+class(ef2test)
 
 
+summary(ef2df)
 
+#Colocar tratamentos em ordem
 em2df$treatment_order <- factor(em2df$treatment_order,
                                     levels = c("agroforestry", "restauration", "forest"))
+#Gráfico fig2_m2
 ggplot(em2df, aes(y = treatment_order, x = prob, color = treatment_order, group = treatment_order)) +
   geom_point(size = 3) +
   geom_errorbarh(aes(xmin = asymp.LCL, xmax = asymp.UCL),
@@ -90,7 +73,3 @@ ggplot(em2df, aes(y = treatment_order, x = prob, color = treatment_order, group 
         strip.background = element_blank(),
         axis.text.x = element_text(angle = 0, hjust = 1),
         legend.position = "none")
-
-
-
-
